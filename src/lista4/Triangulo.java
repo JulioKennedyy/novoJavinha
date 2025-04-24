17aed3af76cf08060aedcd11d13048e5abafe7e1
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-04-23 23:49:00 -0300

    get e set

--- src/lista4/Triangulo.java
+++ src/lista4/Triangulo.java
@@ -1,4 +1,31 @@
 package lista4;
 
 public class Triangulo {
+    private int lado1;
+    private int lado2;
+    private int lado3;
+
+    public int getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(int lado1) {
+        this.lado1 = lado1;
+    }
+
+    public int getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(int lado2) {
+        this.lado2 = lado2;
+    }
+
+    public int getLado3() {
+        return lado3;
+    }
+
+    public void setLado3(int lado3) {
+        this.lado3 = lado3;
+    }
 }
