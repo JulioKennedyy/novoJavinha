e364bd03981420f9552de73df3750a27cb00137f
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-04-27 13:45:50 -0300

    contador

--- src/lista4/Contador.java
+++ src/lista4/Contador.java
@@ -1,4 +1,22 @@
 package lista4;
 
 public class Contador {
+    public float calcularImpostoDeRenda(Trabalhador trabalhador) {
+        float salarioNovo = (float) (trabalhador.getSalarioBruto() - (189.59 * trabalhador.getDependentes()));
+        if (trabalhador.getSalario() < 1903.98) {
+            float valor = (float) getSalario();
+            return valor;
+        }
+        else if (getSalario() < 2826.6) {;
+            return (float) (getSalario() - 142.80);
+        }
+        else if (getSalario() < 3751.05) {
+            return (float) (getSalario() - 354.80);
+        }
+        else if (getSalario() < 4664.68) {
+            return (float) (getSalario() - 636.13);
+        }
+        return (float) (getSalario() - 869.36);
+    }
+    }
 }
--- src/lista4/Trabalhador.java
+++ src/lista4/Trabalhador.java
@@ -1,4 +1,24 @@
 package lista4;
 
 public class Trabalhador {
+    private float salarioBruto;
+    private int dependentes;
+
+    public float getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public void setSalarioBruto(float novoSalarioBruto) {
+        salarioBruto = novoSalarioBruto;
+    }
+
+    public int getDependentes() {
+        return dependentes;
+    }
+
+    public void setDependentes(int novoDependentes) {
+        dependentes = novoDependentes;
+    }
+
+
 }
