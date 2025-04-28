1e103f6dab484b650b467be6caddaf263905df3a
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-04-28 10:39:07 -0300

    finalizado

--- src/lista4/MaquinaDeLavar.java
+++ src/lista4/MaquinaDeLavar.java
@@ -10,9 +10,15 @@ public class MaquinaDeLavar {
             if (i.getCor() == Cor.BRANCO) {
                 verificarRoupas++;
             } else if (i.getCor() == Cor.COLORIDO) {
-                verificarRoupas--;
+                for(Roupa x : roupas) {
+                    if (x.getCor() == Cor.BRANCO) {
+                        verificarRoupas--;
+                    }
+                    else {
+                        verificarRoupas++;
+                    }
+                }
             }
-
             if (i.getTamanho() == Tamanho.P) {
                 tempoDeLavagem += 3;
             }
@@ -35,15 +41,37 @@ public class MaquinaDeLavar {
         if (verificarRoupas < roupas.length) {
             return -1;
         }
-        return tempoDeLavagem;
+        System.out.println("A quantidade de minutos necessario para a lavagem é de: ");
+        return (tempoDeLavagem);
     }
 
     public static void main(String[] args) {
         Scanner print = new Scanner(System.in);
-        Roupa r = new Roupa(Cor.BRANCO,Tamanho.M,Estado.LIMPA);
-        Roupa b = new Roupa(Cor.COLORIDO,Tamanho.P,Estado.LIMPA);
-        Roupa lavagem[] = {r,b};
+        System.out.println("Digite a quantidade de roupas a serem lavadas");
+        int quantidade = (print.nextInt());
+        print.nextLine();
+        Roupa[] roupas = new Roupa[quantidade];
+        for (int i = 0; i < roupas.length; i++) {
+            System.out.print("Digite a cor da roupa (Branco ou colorido): ");
+            String entradaCor = print.nextLine().toUpperCase();
+            Cor cor = Cor.valueOf(entradaCor);
+            System.out.print("Digite o tamanho (P, M, G): ");
+            String entradaTamanho = print.nextLine().toUpperCase();
+            Tamanho tamanho = Tamanho.valueOf(entradaTamanho);
+            System.out.print("Digite o estado da roupa (Limpa ou Suja: ");
+            String entradaEstado = print.nextLine().toUpperCase();
+            Estado estado = Estado.valueOf(entradaEstado);
+            roupas[i] = new Roupa(cor,tamanho,estado);
+        }
         MaquinaDeLavar m = new MaquinaDeLavar();
-        System.out.println(m.lavar(lavagem));
+        System.out.println(m.lavar(roupas));
+        if (m.lavar(roupas) == -1){
+            System.out.println("Não é possivel realizar a lavagem, roupas brancas e coloridas misturadas");
+        }
+        else {
+        for (Roupa x : roupas ) {
+            System.out.println(x.getEstado());
+        }
+        }
     }
 }
