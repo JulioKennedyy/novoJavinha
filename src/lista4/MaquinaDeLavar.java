7fcf43317dba9cff9e903f45482bcaeaccafc3c8
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-04-27 22:45:55 -0300

    as

--- src/lista4/MaquinaDeLavar.java
+++ src/lista4/MaquinaDeLavar.java
@@ -1,4 +1,41 @@
 package lista4;
 
 public class MaquinaDeLavar {
+    public int lavar (Roupa[] roupas) {
+        int verificarRoupas = 0;
+        int tempoDeLavagem = 0;
+        for (Roupa i : roupas) {
+            if (i.getCor() == Cor.BRANCO) {
+                verificarRoupas++;
+            } else if (i.getCor() == Cor.COLORIDO) {
+                verificarRoupas--;
+            }
+            if (verificarRoupas < roupas.length + 1) {
+                return -1;
+            }
+            if (i.getTamanho() == Tamanho.P) {
+                tempoDeLavagem += 3;
+            }
+            else if (i.getTamanho() == Tamanho.M) {
+                tempoDeLavagem += 5;
+            }
+            else if (i.getTamanho() == Tamanho.G) {
+                tempoDeLavagem += 8;
+            }
+            if (i.getCor() == Cor.BRANCO) {
+                tempoDeLavagem++;
+            } else if (i.getCor() == Cor.COLORIDO) {
+                tempoDeLavagem+=2;
+            }
+            if (i.getEstado() == Estado.SUJA) {
+                tempoDeLavagem += 3;
+                i.setEstado(Estado.LIMPA);
+            }
+        }
+        return tempoDeLavagem;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/lista4/Roupa.java
+++ src/lista4/Roupa.java
@@ -4,4 +4,34 @@ public class Roupa {
     private Cor cor;
     private Tamanho tamanho;
     private Estado estado;
+
+    public Cor getCor() {
+        return cor;
+    }
+
+    public void setCor(Cor cor) {
+        this.cor = cor;
+    }
+
+    public Tamanho getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Tamanho tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    Roupa(Cor cor, Tamanho tamanho, Estado estado) {
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.estado = estado;
+    }
 }
