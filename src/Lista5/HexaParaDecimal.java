a5ca408b980f4a7ed584d4062fb9c4eb4b61b26e
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-05-02 22:49:29 -0300

    finalizada

--- src/Lista5/HexaParaDecimal.java
+++ src/Lista5/HexaParaDecimal.java
@@ -1,5 +1,15 @@
 package Lista5;
 
-public class HexaParaDecimal {
+import java.util.Scanner;
 
+public class HexaParaDecimal {
+    public static void main(String[] args) {
+        Scanner print = new Scanner(System.in);
+        System.out.print("Digite o número a ser transformado: ");
+        String hexadecimal = (print.nextLine());
+        int decimal = Integer.parseInt(hexadecimal, 16);
+        int binario = Integer.parseInt(Integer.toBinaryString(decimal));
+        System.out.println("Número decimal: " + decimal);
+        System.out.println("Número em binário: " + binario);
+    }
 }
