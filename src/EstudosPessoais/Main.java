468af83d62a8f68cef70dfdf25785633634bb2dd
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-05-09 22:10:54 -0300

    oioi

--- src/EstudosPessoais/Main.java
+++ src/EstudosPessoais/Main.java
@@ -4,14 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner print = new Scanner(System.in);
-        System.out.println("Digite um número: ");
-        int num = print.nextInt();
-        if (num > 0) {
-            System.out.printf("Seu número é positivo");
-        }
-        else {
-            System.out.println("Número negativo");
+        for(int i = 0; i < 100; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
