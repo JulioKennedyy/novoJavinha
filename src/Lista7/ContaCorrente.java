002d93ade745d200245924dded08c60c69499728
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-05-18 13:47:14 -0300

    costrutor

--- src/Lista7/ContaCorrente.java
+++ src/Lista7/ContaCorrente.java
@@ -4,5 +4,12 @@ public class ContaCorrente {
     private float saldo;
     private String titular;
     private String numero;
-    private float chequeEspecial
+    private float chequeEspecial;
+
+    public ContaCorrente(float saldo, String titular, String numero, float chequeEspecial) {
+        this.saldo = saldo;
+        this.titular = titular;
+        this.numero = numero;
+        this.chequeEspecial = chequeEspecial;
+    }
 }
