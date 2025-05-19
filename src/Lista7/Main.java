3fe0d0fe7e1e8ca2dfd17ac91ee7222c81cf960c
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-05-18 23:31:12 -0300

    8

---
+++ src/Lista7/Main.java
@@ -0,0 +1,9 @@
+package Lista7;
+
+public class Main {
+    public static void main(String[] args) {
+        ContaCorrente[] contasCorrentes = new ContaCorrente[50];
+        ContaPoupanca[] contasPoupancas = new ContaPoupanca[50];
+        
+    }
+}
