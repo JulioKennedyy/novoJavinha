208625d555321c737766e7ed0648c8ec09e5a043
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-06-02 23:07:35 -0300

    4 pronta

--- src/Lista8/Main.java
+++ src/Lista8/Main.java
@@ -18,6 +18,40 @@ public class Main {
                     soma.setOperando1(valor1);
                     soma.setOperando2(valor2);
                     soma.imprimirResultado();
+                    break;
+                case "subtrair":
+                    System.out.println("Digite o primeiro valor: ");
+                    Subtrair sub = new Subtrair();
+                    int valor3 = scan.nextInt();
+                    System.out.println("Digite o segundo valor: ");
+                    int valor4 = scan.nextInt();
+                    sub.setOperando1(valor3);
+                    sub.setOperando2(valor4);
+                    sub.imprimirResultado();
+                    break;
+                case "multiplicar":
+                    System.out.println("Digite o primeiro valor: ");
+                    Multiplicar mul = new Multiplicar();
+                    int valor5 = scan.nextInt();
+                    System.out.println("Digite o segundo valor: ");
+                    int valor6 = scan.nextInt();
+                    mul.setOperando1(valor5);
+                    mul.setOperando2(valor6);
+                    mul.imprimirResultado();
+                    break;
+                case "dividir":
+                    System.out.println("Digite o primeiro valor: ");
+                    Dividir div = new Dividir();
+                    int valor7 = scan.nextInt();
+                    System.out.println("Digite o segundo valor: ");
+                    int valor8 = scan.nextInt();
+                    div.setOperando1(valor7);
+                    div.setOperando2(valor8);
+                    div.imprimirResultado();
+                    break;
+                default:
+                    System.out.println("Não pode");
+                    break;
             }
         }
     }
