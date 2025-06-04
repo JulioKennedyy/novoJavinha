09c1bc96bf50ce86b404ed1e2c58c2acb07a0425
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-06-04 10:52:18 -0300

    problema resolvido

--- src/EstudoXml/Estoque.java
+++ src/EstudoXml/Estoque.java
@@ -1,4 +1,27 @@
 package EstudoXml;
 
 public class Estoque {
+    private String nomeDoProduto;
+    private float precoDoProduto;
+
+    public Estoque(String nomeDoProduto, float precoDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+        this.precoDoProduto = precoDoProduto;
+    }
+
+    public String getNomeDoProduto() {
+        return nomeDoProduto;
+    }
+
+    public void setNomeDoProduto(String nomeDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+    }
+
+    public float getPrecoDoProduto() {
+        return precoDoProduto;
+    }
+
+    public void setPrecoDoProduto(float precoDoProduto) {
+        this.precoDoProduto = precoDoProduto;
+    }
 }
--- src/EstudoXml/SalvarDados.java
+++ src/EstudoXml/SalvarDados.java
@@ -1,4 +1,12 @@
 package EstudoXml;
 
+import com.thoughtworks.xstream.XStream;
+
 public class SalvarDados {
+    public static void main(String[] args) {
+        XStream xstream = new XStream();
+        Estoque e = new Estoque("Macaxeira", 5);
+        String xml = xstream.toXML(e);
+        System.out.println(xml);
+    }
 }
