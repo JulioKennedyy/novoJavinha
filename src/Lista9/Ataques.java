f279a4142170b4bc6c1e9d536ff2ccde9049c0b2
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-06-05 09:27:47 -0300

    processos

---
+++ src/Lista9/Ataques.java
@@ -0,0 +1,5 @@
+package Lista9;
+
+public interface Ataques {
+    void movimento();
+}
--- src/Lista9/Pokemon.java
+++ src/Lista9/Pokemon.java
@@ -1,8 +1,37 @@
 package Lista9;
 
-public class Pokemon {
+public class Pokemon implements Ataques{
     private String nome;
     private String tipo;
     private int nivel;
 
+    public Pokemon(String nome, String tipo, int nivel) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.nivel = nivel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
 }
