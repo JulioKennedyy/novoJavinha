74babea581698269ba8b4ed1b5321111375d5085
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-06-16 09:27:32 -0300

    questao 1 feita

--- src/Lista10/quest1/Mensageiro.java
+++ src/Lista10/quest1/Mensageiro.java
@@ -1,4 +1,8 @@
 package Lista10.quest1;
 
 public class Mensageiro {
+    public Mensagem prepararMensagem(String mensagem, String destino) {
+        Mensagem m = new Mensagem(mensagem);
+        return m;
+    }
 }
--- src/Lista10/quest1/Mensagem.java
+++ src/Lista10/quest1/Mensagem.java
@@ -1,4 +1,22 @@
 package Lista10.quest1;
 
 public class Mensagem {
+    private String texto;
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensagem: " + texto;
+    }
+
+    public Mensagem(String texto) {
+        this.texto = texto;
+    }
 }
