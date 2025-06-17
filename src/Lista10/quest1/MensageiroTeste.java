e1a04c13602ef11133362f8822c55c808d51d836
Joulio Kennedy <kennedy.julio@academico.ifpb.edu.br>
2025-06-17 09:28:18 -0300

    3 pronta

--- src/Lista10/quest1/MensageiroTeste.java
+++ src/Lista10/quest1/MensageiroTeste.java
@@ -1,4 +1,22 @@
 package Lista10.quest1;
 
+import java.util.Scanner;
+
 public class MensageiroTeste {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Mensageiro m = new Mensageiro();
+        System.out.println("Digite a mensagem: ");
+        String texto = scan.nextLine();
+        System.out.println("Digite se a mensagem é externo ou interno:");
+        String tipo = scan.nextLine();
+        if(tipo.equals("externo")) {
+            Mensagem externo = m.prepararMensagem(texto, "externo");
+            System.out.println("Externa: " + externo.getTexto());
+        }
+        else {
+            Mensagem interna = m.prepararMensagem(texto, "interno");
+            System.out.println("Externa: " + interna.getTexto());
+        }
+    }
 }
